916a9a011ae8d37822f3556df81ac3e04d936e6f
Himanshu <57253980+himanshu07121999@users.noreply.github.com>
2024-08-25 11:29:44 +0530

    Add files via upload

---
+++ StrategyDesignPattern/Strategy/SportDriveStrategy.java
@@ -0,0 +1,7 @@
+package StrategyDesignPattern.Strategy;
+
+public class SportDriveStrategy implements DriveStrategy{
+	public void drive() {
+		System.out.println("Sport Drive Capability");
+	}
+}
---
+++ StrategyDesignPattern/Strategy/TrackVehicleCapability.java
@@ -0,0 +1,8 @@
+package StrategyDesignPattern.Strategy;
+
+public class TrackVehicleCapability implements DriveStrategy{
+	public  void drive() {
+		System.out.println("Track Vehicle capabilty");
+
+	}
+}
